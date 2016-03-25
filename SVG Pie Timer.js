e4b56acb6e2f9fe3c1f9fd7fbd2dbd92b92b33fb
Patricia Rollins <patrol7171@users.noreply.github.com>
2016-03-25 17:35:43 -0400

    Update SVG Pie Timer.js

--- SVG Pie Timer.js
+++ SVG Pie Timer.js
@@ -112,7 +112,7 @@ svgPieTimer({
     element: [loader, border],
     
     // Duration (Optional) In milliseconds. Defaults to 1000.
-    duration: 60000 * pieChartMin,
+    duration: 60000 * 5,
     
     // Loops (Optional) Defaults to 1. Set to 0 for infinite.
     loops: 1
